0b0e01e268bc5930f53efc50a739b6a9cf2997c7
Kim Do Ky <makingfunk0@gmail.com>
2018-01-15 23:53:32 +0900

    blog.models.Post에 photo ImageField 추가

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Post(models.Model):
 	title = models.CharField(max_length=100)
 	content = models.TextField()
+	photo = models.ImageField()
 	is_public = models.BooleanField(default=False)
 	create_at = models.DateTimeField(auto_now_add=True)
 	update_at = models.DateTimeField(auto_now=True)
