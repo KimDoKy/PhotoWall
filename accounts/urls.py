9039f49d60da995282ae45aceb792ee6e8c5db9f
Kim Do Ky <makingfunk0@gmail.com>
2018-01-18 13:19:09 +0900

    로그인 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,8 @@
-from django.conf.urls import url
+from django.urls import path
+from django.contrib.auth.views import login
 
 urlpatterns = [
+	path('login/', login, name='login', kwargs={
+			'template_name': 'accounts/login_form.html',
+			}),
 	]
