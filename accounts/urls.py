35711a4cdc4afe4d65dfdcdac6499ed4144b1595
Kim Do Ky <makingfunk0@gmail.com>
2018-01-18 22:15:57 +0900

    로그아웃을 구현하고, 로그인/로그아웃에 next 인자 지정

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from django.contrib.auth.views import login
+from django.contrib.auth.views import login, logout
 from . import views
 
 urlpatterns = [
 	path('login/', login, name='login', kwargs={
 			'template_name': 'accounts/login_form.html',
 			}),
+	path('logout/', logout, name='logout', kwargs={'next_page': 'login',}),
 	path('signup/', views.signup, name='signup'),
 	path('profile/', views.profile, name='profile'),
 	]
