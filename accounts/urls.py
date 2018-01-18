27558ccee6a45e84f49fb2e1a748dd56f928b407
Kim Do Ky <makingfunk0@gmail.com>
2018-01-18 21:23:36 +0900

    회원가입 기능 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from django.contrib.auth.views import login
+from . import views
 
 urlpatterns = [
 	path('login/', login, name='login', kwargs={
 			'template_name': 'accounts/login_form.html',
 			}),
+	path('signup/', views.signup, name='signup'),
 	]
