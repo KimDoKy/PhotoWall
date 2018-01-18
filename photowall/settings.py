32215fa2e9d387e16fca236e659447ffd81509a4
Kim Do Ky <makingfunk0@gmail.com>
2018-01-18 21:50:38 +0900

    프로젝트 단위 템플릿 구현

--- photowall/settings.py
+++ photowall/settings.py
@@ -59,7 +59,9 @@ ROOT_URLCONF = 'photowall.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+		    os.path.join(BASE_DIR, 'photowall', 'templates'),
+		],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
